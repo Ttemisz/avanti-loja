f12e1f572f20f6cd27179856382115dc4c557500
Temistoclis <temistoclisxp58@gmail.com>
2025-04-11 23:06:53 -0300

    segundo commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
+function AbrirHamburguer() {
+    var Departamento = document.getElementById("Departamentos");
 
+    if (Departamento.style.display === "flex") {
+        Departamento.style.display = "none";
+      } else {
+        Departamento.style.display = "flex";
+      }
+}
